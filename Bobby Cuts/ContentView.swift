a5fbf09b389ccda83d0b5a3c95b82926bf24c8b9
Havish Prattipati <havishp@gmail.com>
2025-10-06 14:47:21 -0400

    Initial Commit

---
+++ Bobby Cuts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bobby Cuts
+//
+//  Created by Havish on 10/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
